7e9b47b939c4c109124191414abc36de7b081d04
Antoine <mahassadijoseph@gmail.com>
2024-03-21 21:52:23 +0100

    edit

--- gsap.js
+++ gsap.js
@@ -2,17 +2,30 @@ gsap.registerPlugin(ScrollTrigger);
 
 const $Acc = document.querySelector("#Accueil");
 const $Propos = document.querySelector("#A-propos");
+const $a = document.querySelectorAll("#scrollY");
+
 
 gsap.to($Propos, {
     y: "+=-100vh",
     scrollTrigger: {
         trigger: $Acc,
-        markers: true,
-        start: "30%; 20%",
-        end: "60%; 45%",
+        markers: false,
+        start: "20%; 20%",
+        end: "55%; 45%",
         scrub: 2,
         toggleActions: "play reverse play reverse",
 
     }
 })
 
+gsap.to($a, {
+    color: "black",
+    scrollTrigger: {
+        trigger: $Acc,
+        start: "20%; 20%",
+        markers: true,
+        end: "20%",
+        toggleActions: "play none reverse none",
+    }
+}
+)
